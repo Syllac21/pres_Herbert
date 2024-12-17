e66e139b6eace3c94f9ed73727b510b994acf9e4
Syllac21 <syllac.online@gmail.com>
2024-12-17 10:25:46 +0100

    gestion du formulaire

--- scripts.js
+++ scripts.js
@@ -1,14 +1,20 @@
+// DOM secret
 const buttonSecret = document.getElementById('show');
 const divSecret = document.getElementById('secret');
 let showSecret = false
 
-// modale
+// DOM modale
 const modal = document.querySelector('.galerie__modal');
 const modalImg = document.getElementById('modalImage');
 const captionText = document.getElementById('caption');
 const closeBtn = document.querySelector('.close');
 const images = document.querySelectorAll('.gallery img');
 
+// DOM formulaire
+const forumulaire = document.querySelector('.form');
+const formMessage = document.querySelector('.form__message');
+
+// gestion du secret
 clickShow = buttonSecret.addEventListener('click', ()=>{
     if(showSecret){
         divSecret.classList.add('hidden');
@@ -21,13 +27,14 @@ clickShow = buttonSecret.addEventListener('click', ()=>{
     }
 })
 
+// gestion de la modale
 images.forEach(image => {
     image.addEventListener('click', function() {
         modal.style.display = "flex";
         modal.classList.remove('hidden');
         modalImg.src = this.src;
         modalImg.alt = this.alt; // Mettre l'image dans la modale
-        // captionText.innerHTML = this.alt; // Ajouter la légende de l'image
+        
     });
 });
 
@@ -40,3 +47,47 @@ window.addEventListener('click', function(event) {
         modal.style.display = "none";
     }
 });
+
+// gestion du formulaire
+forumulaire.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    formMessage.innerHTML = "";
+    formMessage.classList.remove('success','error');
+
+    const name = document.getElementById('name').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const message = document.getElementById('message').value.trim();
+
+    // tous les champs sont remplis
+    if(
+        !name ||
+        !email ||
+        !message
+    ){
+        formMessage.innerHTML = 'Tous les champs sont obligatoires';
+        formMessage.classList.add('error');
+        return;
+    }
+
+    // le message fait plus de 50 caractères
+    if(message.length < 50){
+        formMessage.innerHTML = 'le message doit comporter au moins 50 caractères';
+        formMessage.classList.add('error');
+        return
+    }
+
+    // l'addresse mail est valide
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)){
+        formMessage.innerHTML = 'Veuillez saisir un email valide';
+        formMessage.classList.add('error');
+        return
+    }
+
+    formMessage.innerHTML = 'votre message a été envoyé';
+    formMessage.classList.add('success');
+
+    forumulaire.reset();
+
+})
